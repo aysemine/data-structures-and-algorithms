ef201088146389e170381d5c34beabc4dd3e99c7
aysemine <93674435+aysemine@users.noreply.github.com>
2024-01-06 13:42:28 +0300

    Add files via upload

---
+++ mergeSort.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int middle, int right) {
+    int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[middle + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+        printf("Divide: ");
+        for (int i = left; i <= right; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        merge(arr, left, middle, right);
+
+        printf("Merge: ");
+        for (int i = left; i <= right; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {16, 21, 11, 8, 12, 22};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, arrSize);
+
+    mergeSort(arr, 0, arrSize - 1);
+
+    printf("Sorted array: ");
+    printArray(arr, arrSize);
+
+    return 0;
+}
