0b067df3f70e77ab7b486a107c4f9648c9500834
aysemine <93674435+aysemine@users.noreply.github.com>
2024-01-06 13:33:40 +0300

    Add files via upload

---
+++ insertionSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        printf("Current Key: %d\n", key);
+        printf("Array Before Sorting: ");
+        printArray(arr, n);
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+
+        printf("Array After Sorting: ");
+        printArray(arr, n);
+        printf("\n");
+    }
+}
+
+int main() {
+    int arr[] = {7, 3, 5, 8, 2, 9, 4, 15, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    
+    printf("Sorted Array: ");
+    printArray(arr, n);
+
+    return 0;
+}
